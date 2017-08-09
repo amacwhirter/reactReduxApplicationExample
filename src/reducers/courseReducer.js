2f973fb99ef3371162a4c2759bd597a22ddc3faf
Ashley MacWhirter <macwhirtera@gmail.com>
2017-08-09 14:26:48 -0400

    fix actiontype import in reducer

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,4 +1,4 @@
-import * as types from './action/actionTypes';
+import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
   switch(action.type) {
